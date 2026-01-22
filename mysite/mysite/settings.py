3f53f4d26dfe54b9c9835814fd7aec40505bc08f
oattao123 <dceriythrrmkic@gmail.com>
2026-01-22 11:22:54 +0700

    Add core app with premium design and update settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_vizx(urde+wnfgj&46sqtg89f#f%uzk+2-__(6e3(kh749w1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oattao123.pythonanywhere.com']
+ALLOWED_HOSTS = ['oattao123.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
